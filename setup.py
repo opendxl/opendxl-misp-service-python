a28f3e977059b0c5ee9d6e2f85c0c297f0ce4855
Jeremy Barlow <jeremy_barlow@mcafee.com>
2018-04-05 08:49:24 -0700

    Added pylint and Travis configuration

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
+# pylint: disable=no-member, no-name-in-module, import-error
+
 from __future__ import absolute_import
+import glob
 import os
 import distutils.command.sdist
-from setuptools import setup
+import distutils.log
+import subprocess
+from setuptools import Command, setup
 import setuptools.command.sdist
 
 # Patch setuptools' sdist behaviour with distutils' sdist behaviour
@@ -10,7 +15,47 @@ setuptools.command.sdist.sdist.run = distutils.command.sdist.sdist.run
 VERSION_INFO = {}
 CWD = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(CWD, "dxlmispservice", "_version.py")) as f:
-    exec(f.read(), VERSION_INFO)
+    exec(f.read(), VERSION_INFO)  # pylint: disable=exec-used
+
+
+class LintCommand(Command):
+    """
+    Custom setuptools command for running lint
+    """
+    description = 'run lint against project source files'
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        self.announce("Running pylint for library source files and tests",
+                      level=distutils.log.INFO)
+        subprocess.check_call(["pylint", "dxlmispservice"] +
+                              glob.glob("*.py"))
+        self.announce("Running pylint for samples", level=distutils.log.INFO)
+        subprocess.check_call(["pylint"] + glob.glob("sample/*.py") +
+                              glob.glob("sample/**/*.py") +
+                              ["--rcfile", ".pylintrc.samples"])
+
+
+class CiCommand(Command):
+    """
+    Custom setuptools command for running steps that are performed during
+    Continuous Integration testing.
+    """
+    description = 'run CI steps (lint, test, etc.)'
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        self.run_command("lint")
+
+TEST_REQUIREMENTS = ["pylint"]
+
+DEV_REQUIREMENTS = TEST_REQUIREMENTS + ["sphinx"]
 
 setup(
     # Package name:
@@ -19,7 +64,7 @@ setup(
     # Version number:
     version=VERSION_INFO["__version__"],
 
-    # Package requirements
+    # Requirements
     install_requires=[
         "dxlbootstrap>=0.1.3",
         "dxlclient",
@@ -27,17 +72,21 @@ setup(
         "pyzmq"
     ],
 
-    # Python version requirements
-    python_requires=">=2.7.9,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*",
+    tests_require=TEST_REQUIREMENTS,
+
+    extras_require={
+        "dev": DEV_REQUIREMENTS,
+        "test": TEST_REQUIREMENTS
+    },
 
     # Package author details:
-    author="",
+    author="McAfee LLC",
 
     # License
-    license="",
+    license="Apache License 2.0",
 
     # Keywords
-    keywords=[],
+    keywords=['opendxl', 'dxl', 'mcafee', 'service', 'misp'],
 
     # Packages
     packages=[
@@ -51,19 +100,29 @@ setup(
         "dxlmispservice._config.app" : ['*']},
 
     # Details
-    url="",
+    url="http://www.mcafee.com",
 
-    description="",
+    description="MISP DXL Python service library",
 
     long_description=open('README').read(),
 
+    python_requires='>=2.7.9,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
+
     classifiers=[
-        "Programming Language : Python",
-        "Programming Language : Python :: 2",
-        "Programming Language : Python :: 2.7",
-        "Programming Language : Python :: 3",
-        "Programming Language : Python :: 3.4",
-        "Programming Language : Python :: 3.5",
-        "Programming Language : Python :: 3.6"
+        "Development Status :: 4 - Beta",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6"
     ],
+
+    cmdclass={
+        "ci": CiCommand,
+        "lint": LintCommand
+    }
 )

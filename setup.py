7bc79547db3a6bf747b5cebd7dbcf1b7d5110e75
Jeremy Barlow <jeremy_barlow@mcafee.com>
2018-04-23 15:40:05 -0700

    Bump minimum bootstrap and client dependencies for Python 3

--- setup.py
+++ setup.py
@@ -67,8 +67,8 @@ setup(
 
     # Requirements
     install_requires=[
-        "dxlbootstrap>=0.1.3",
-        "dxlclient",
+        "dxlbootstrap>=0.2.0",
+        "dxlclient>=4.1.0.184",
         "pymisp",
         "pyzmq"
     ],

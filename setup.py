42e1b2d31c6794dfd7075df7c05b346c223d762e
Chris Smith <csmith5@mcafee.com>
2019-09-27 13:25:27 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ class CiCommand(Command):
         self.run_command("lint")
         self.run_command("test")
 
-TEST_REQUIREMENTS = ["mock", "nose", "pylint", "requests-mock"]
+TEST_REQUIREMENTS = ["mock", "nose", "requests-mock", "astroid<2.3.0", "pylint<=2.3.1"]
 
 DEV_REQUIREMENTS = TEST_REQUIREMENTS + ["sphinx"]
 
